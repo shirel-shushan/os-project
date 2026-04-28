f6c9a24cc3727d576af12311e85ea9c46bce6a85
Shirel Shushan <ramot0852@gmail.com>
2026-04-28 20:54:54 +0300

    update main

--- main.c
+++ main.c
@@ -39,50 +39,40 @@ int main() {
         addEdge(g, src, dst, weight);
     }
 
-//     int start, end;
-//     if (fscanf(file, "%d %d", &start, &end) != 2 ||
-//         start < 0 || end < 0 || start >= N || end >= N) {
-//         printf("Invalid input\n");
-//         freeGraph(g);
-//         fclose(file);
-//         return 1;
-//     }
-//     fclose(file);
+    int start, end;
+    if (fscanf(file, "%d %d", &start, &end) != 2 ||
+        start < 0 || end < 0 || start >= N || end >= N) {
+        printf("Invalid input\n");
+        freeGraph(g);
+        fclose(file);
+        return 1;
+    }
+    fclose(file);
 
-//     // מקרה מקור == יעד
-//    if (start == end) {
-//     printf("%d\n0\n", start,0);
-//     freeGraph(g);
-//     return 0;
-// }
-
-//     int pathLen, totalWeight;
-//     int* path = dijkstra(g, start, end, &pathLen, &totalWeight);
-
-//     if (path == NULL) {
-//         printf("No path found\n");
-//     } else {
-//         for (int i = 0; i < pathLen; i++) {
-//             if (i < pathLen - 1)
-//                 printf("%d -> ", path[i]);
-//             else
-//                 printf("%d", path[i]);
-//         }
-//         printf("\n%d\n", totalWeight);
-//         free(path);
-//     }
-
-//     freeGraph(g);
-//     return 0;
-// }
-printf("Weight 0->1: %d\n", g->graph[0][1]);
-printf("Weight 0->2: %d\n", g->graph[0][2]);
-printf("Weight 2->1: %d\n", g->graph[2][1]);
-printf("Weight 4->5: %d\n", g->graph[4][5]);
-printf("Weight 1->5: %d\n", g->graph[1][5]);
-fclose(file);
+    // מקרה מקור == יעד
+   if (start == end) {
+    printf("%d\n0\n", start,0);
     freeGraph(g);
-    
-    printf("Graph loaded and freed successfully!\n"); 
     return 0;
 }
+
+    int pathLen, totalWeight;
+    int* path = dijkstra(g, start, end, &pathLen, &totalWeight);
+
+    if (path == NULL) {
+        printf("No path found\n");
+    } else {
+        for (int i = 0; i < pathLen; i++) {
+            if (i < pathLen - 1)
+                printf("%d -> ", path[i]);
+            else
+                printf("%d", path[i]);
+        }
+        printf("\n%d\n", totalWeight);
+        free(path);
+    }
+
+    freeGraph(g);
+    return 0;
+}
+
